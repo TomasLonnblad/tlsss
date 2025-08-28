32f55b28b351f4e7739fbd14e3af3bdafd66869d
TomasLonnblad <tomaslonnblad@gmail.com>
2025-08-28 10:37:40 +0200

    x

---
+++ src/app/page.tsx
@@ -0,0 +1,41 @@
+
+export default function Home() {
+
+return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-20 p-24 bg-neutral-900 text-neutral-100">
+<div className="relative">
+<button className="relative bg-neutral-700 px-3 py-2 rounded-md hover:bg-neutral-800 transition-colors duration-200 z-10">
+
+   <footer>
+        <a
+          href="https://tomaslonnblad.github.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Nextjs{" "}
+          <img src="/vercel.svg" alt="" className="" />
+        </a>
+ 
+        <a
+          href="https://mux.com?utm_source=create-next-app&utm_medium=with-hls-js&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+           <img src="/mux.svg" alt="" className="" />
+        </a>
+      </footer>
+
+</button>
+
+
+<div 
+className="absolute -inset-[1px] rounded-md"
+style={{
+  background:
+"conic-gradient(#ff4545, #00ff99, #006aff, #ff0095, #ff4545"
+}}
+/>
+</div>
+</main>
+
+)}
